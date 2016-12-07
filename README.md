c8bfa046d36b4802c585eef9397ee84eeca13ca7
bahelms <jimbonkgit@gmail.com>
2016-12-07 11:21:35 -0500

    Adds `stop` and documentation for `burndown`

--- README.md
+++ README.md
@@ -43,3 +43,19 @@ OR
 
     No currently running task
 
+#### Show history of tasks -- today is default
+
+    tracker burndown
+
+returns
+
+    -- <job_name>
+      Total Duration: 4 hours, 16 minutes
+      Sessions:
+        12:34 - 03:14 PM EST
+        04:00 - 04:30 PM EST
+
+    -- <job_name>
+      Total Duration: 30 minutes
+      Sessions:
+        08:15 - 08:30 AM EST
--- tracker.py
+++ tracker.py
@@ -11,7 +11,7 @@ def cli():
 
 @cli.command()
 def status():
-    """Status of the currently tracked task."""
+    """Status of the currently tracked task"""
     click.echo("No currently running tasks.")
 
 
@@ -21,3 +21,14 @@ def start(task):
     """Start tracking a task"""
     start_date = "12/06/16 04:23 PM EST"
     click.echo("{} started at {}".format(task, start_date))
+
+
+@cli.command()
+def stop():
+    """Stop tracking the current task"""
+    result = """
+    <job_name> stopped
+        Time: 11:30 AM -- 02:31 PM EST
+    Duration: 3 hours, 1 minute
+    """
+    click.echo(result)
